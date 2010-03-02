5882d19343485a6ed792ad1014e2420f13ccec65
casein <webcasein@gmail.com>
2010-03-02 16:13:56 -0600

    getting maps to work

--- app/controllers/county_controller.rb
+++ app/controllers/county_controller.rb
@@ -76,8 +76,12 @@ class CountyController < ApplicationController
     @map.control_init(:large_map => true, :map_type => true)
     @map.center_zoom_init([43.388957,-88.252144],10)
     #map attributes
-    points = GMarker.new([43.388957,-88.252144], :title => "hello", :info_window => "hola")
-    @map.overlay_init(points)
+    @pPoints = Map.active.coordinates
+    @pArray = @pPoints.inject([]) {|result, element| result.push([element.xCoord, element.yCoord])}
+#    mInterp = GMarker.new([@pArray[0]], :title => "hello2", :info_window => "hola")
+#    mHard = GMarker.new([43.300000,-88.300000], :title => "hello", :info_window => "43.388957,-88.252144")
+#    @map.overlay_init(mInterp)
+#    @map.overlay_init(mHard)
   end
   
 private
--- app/models/map.rb
+++ app/models/map.rb
@@ -4,5 +4,16 @@ class Map < ActiveRecord::Base
   
     named_scope :active, :conditions => ['maps.pointActive = ?', 1]
     
-    named_scope :coordinates, :conditions => ["pointXCoord > ? AND pointYCoord > ?", 0, 0]
+    named_scope :byCounty, lambda { |county|
+      {
+        :joins => "INNER JOIN transects on maps.transect_id = transects.id", :conditions => ['transects.transectCountyName = ?', county]
+      } }
+      
+      named_scope :byYear, lambda { |r2y| 
+      { 
+        :joins => "INNER JOIN seasons on maps.id = seasons.point_id", :conditions => ['seasons.seasonYear = ?', r2y] 
+       } }
+    
+    named_scope :coordinates, :conditions => ["maps.pointXCoord > ? AND maps.pointYCoord > ?", 0, 0]
+    
 end
