18a34345532d544282ce9568e2bc6b4732f49e07
casein <webcasein@gmail.com>
2010-03-02 11:09:36 -0600

    adding maps model

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,2 +1,8 @@
 class Map < ActiveRecord::Base
+  belongs_to :route_stop
+  has_many :seasons
+  
+    named_scope :active, :conditions => ['maps.pointActive = ?', 1]
+    
+    named_scope :coordinates, :conditions => ["xCoord > ? AND yCoord > ?", 0, 0]
 end
