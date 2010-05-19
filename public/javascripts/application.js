f88b8be7b5814640a8497fd29a91b6291922db80
casein <webcasein@gmail.com>
2010-05-19 12:57:02 -0500

    loading gif

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-//function noticeFade()
-//{
-//new Effect.Fade ('notice', { duration: 3.0});
-//}
-
-//window.onload = noticeFade;
 
 Event.observe(window, 'load', function(){
 	$A(document.getElementsByClassName('notice')).each(function(o) {
@@ -17,61 +11,14 @@ Event.observe(window, 'load', function(){
 
 
 
-var DDSPEED = 10;
-var DDTIMER = 15;
 
-// main function to handle the mouse events //
-function ddMenu(id,d){
-  var h = document.getElementById(id + '-ddheader');
-  var c = document.getElementById(id + '-ddcontent');
-  clearInterval(c.timer);
-  if(d == 1){
-    clearTimeout(h.timer);
-    if(c.maxh && c.maxh <= c.offsetHeight){return}
-    else if(!c.maxh){
-      c.style.display = 'block';
-      c.style.height = 'auto';
-      c.maxh = c.offsetHeight;
-      c.style.height = '0px';
-    }
-    c.timer = setInterval(function(){ddSlide(c,1)},DDTIMER);
-  }else{
-    h.timer = setTimeout(function(){ddCollapse(c)},50);
+Ajax.Responders.register({
+  onCreate: function() {
+    if($('busy') && Ajax.activeRequestCount>0)
+      Effect.Appear('busy',{duration:0.5,queue:'end'});
+  },
+  onComplete: function() {
+    if($('busy') && Ajax.activeRequestCount==0)
+      Effect.Fade('busy',{duration:0.5,queue:'end'});
   }
-}
-
-// collapse the menu //
-function ddCollapse(c){
-  c.timer = setInterval(function(){ddSlide(c,-1)},DDTIMER);
-}
-
-// cancel the collapse if a user rolls over the dropdown //
-function cancelHide(id){
-  var h = document.getElementById(id + '-ddheader');
-  var c = document.getElementById(id + '-ddcontent');
-  clearTimeout(h.timer);
-  clearInterval(c.timer);
-  if(c.offsetHeight < c.maxh){
-    c.timer = setInterval(function(){ddSlide(c,1)},DDTIMER);
-  }
-}
-
-// incrementally expand/contract the dropdown and change the opacity //
-function ddSlide(c,d){
-  var currh = c.offsetHeight;
-  var dist;
-  if(d == 1){
-    dist = (Math.round((c.maxh - currh) / DDSPEED));
-  }else{
-    dist = (Math.round(currh / DDSPEED));
-  }
-  if(dist <= 1 && d == 1){
-    dist = 1;
-  }
-  c.style.height = currh + (dist * d) + 'px';
-  c.style.opacity = currh / c.maxh;
-  c.style.filter = 'alpha(opacity=' + (currh * 100 / c.maxh) + ')';
-  if((currh < 2 && d != 1) || (currh > (c.maxh - 2) && d == 1)){
-    clearInterval(c.timer);
-  }
-}
+});
